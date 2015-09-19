b964f7277a1af63d594197dac2a7ac293a497ddb
Austin Horning <austinseven@outlook.com>
2015-09-19 14:13:06 -0400

    Prepare for supportkit

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'unicorn'
 gem 'unicorn-rails'
 gem 'faraday'
 gem 'faraday_middleware'
+gem 'dropbox-sdk'
 group :development do
   gem 'better_errors'
   gem 'guard-bundler'
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   resources :projects
   devise_for :organizations
-  resources :users
-  resources :projects
-  resources :organizations
   
   root to: 'visitors#index'
   get '/auth/:provider/callback' => 'sessions#create'
@@ -16,4 +13,15 @@ Rails.application.routes.draw do
   get '/developer/:id/dash' => 'users#index'
   get '/developer/:id/settings' => 'users#edit'
   post '/developer/:id/update' => 'users#update'
+
+  #organizationland
+  get '/organization/:id' => 'organizations#show'
+  get '/organization/:id/dash' => 'organizations#index'
+
+  #projectland
+  get '/project/:id' => 'projects#show'
+  get '/project/:id/edit' => 'projects#edit'
+  post '/project/:id/update' => 'projects#update'
+  get '/project/new' => 'projects#new'
+  post '/project/create' => 'projects#create'
 end
