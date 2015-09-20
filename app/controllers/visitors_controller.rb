687318657678eccada94e5eeafdc7beea992c07a
Giorgio Delgado <delg4200@mylaurier.ca>
2015-09-20 02:45:41 -0400

    added organization stuffs

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,7 +1,7 @@
 class VisitorsController < ApplicationController
 	def index
 		if organization_signed_in?
-
+			redirect_to "/organization/#{current_organization.id}/dash"
 		elsif user_signed_in?
 
 		end
